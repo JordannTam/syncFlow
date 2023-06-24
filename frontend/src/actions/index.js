e4d6743e7ed05bdf45b802cc7ad934d1a91164f5
Jordan Tam <z5254001@ad.unsw.edu.au>
2023-06-25 07:35:38 +1000

    Set up redux, edit task list functionality

---
+++ frontend/src/actions/index.js
@@ -0,0 +1,56 @@
+// **************
+// Task Functions
+// **************
+
+export const addTask = (object) => {
+    return {
+        type: "ADD_TASK",
+        id: object.id,
+        title: object.title,
+        description: object.description,
+        deadline: object.deadline,
+        assignee: object.assignee,
+    };
+};
+export const editTask = (object) => {
+    return {
+        type: "EDIT_TASK",
+        id: object.id,
+        title: object.title,
+        description: object.description,
+        deadline: object.deadline,
+        assignee: object.assignee,
+        state: object.state,
+    };
+};
+
+export const changeTaskState = (id, state) => {
+    return {
+        type: "CHANGE_TASK_STATE",
+        id,
+        state,
+    };
+};
+
+export const setTasks = (tasks) => {
+    return {
+        type: "SET_TASK",
+        tasks,
+    };
+};
+
+// **************
+// Login Function
+// **************
+
+export const login = () => {
+    return {
+        type: "LOGIN",
+    };
+};
+
+export const logout = () => {
+    return {
+        type: "LOGOUT",
+    };
+};
