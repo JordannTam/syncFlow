0ca5051b1ceb3b028ed3bafdc6f9959d1321baa8
Zihang Xu <optimistic@Zihang-2.modem>
2023-07-05 01:08:03 +1000

    Connection relate stuff

--- backend/setup.sql
+++ backend/setup.sql
@@ -26,6 +26,24 @@ CREATE TABLE task_assignees (
     PRIMARY KEY (task_id, profile_id)
 );
 
+CREATE TABLE connections (
+    id1 INTEGER NOT NULL,
+    id2 INTEGER NOT NULL,
+    PRIMARY KEY (id1, id2),
+    FOREIGN KEY (id1) REFERENCES profiles(id) ON DELETE CASCADE,
+    FOREIGN KEY (id2) REFERENCES profiles(id) ON DELETE CASCADE
+);
+
+CREATE TABLE connection_requests (
+    id1 INTEGER NOT NULL,
+    id2 INTEGER NOT NULL,
+    PRIMARY KEY (id1, id2),
+    FOREIGN KEY (id1) REFERENCES profiles(id) ON DELETE CASCADE,
+    FOREIGN KEY (id2) REFERENCES profiles(id) ON DELETE CASCADE
+);
+
 ALTER table profiles owner to teamendgame;
 ALTER table tasks owner to teamendgame;
 ALTER table task_assignees owner to teamendgame;
+ALTER table connections owner to teamendgame;
+ALTER table connection_requests owner to teamendgame;
